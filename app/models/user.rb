e8a99d3a949b10bcf057ea5d5c0358acafbba0d3
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-18 17:50:28 -0700

    User story 10 functionality
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>
    Co-Authored-By: Paul Debevec <PaulDebevec@users.noreply.github.com>

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates_presence_of :name, :address, :city, :state, :zip, :password
+  validates :email, presence: true, uniqueness: true
+  has_secure_password
+end
