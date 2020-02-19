65a780f136cf7ee3016deacfbcb5c1fcd9b524d2
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-18 18:43:12 -0700

    Test for user story 12 unique email
    Co-Authored-By: Maria <mronauli@users.noreply.github.com>
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>
    Co-Authored-By: Paul Debevec <PaulDebevec@users.noreply.github.com>

--- spec/features/user/new_spec.rb
+++ spec/features/user/new_spec.rb
@@ -59,5 +59,45 @@ RSpec.describe "New User Form" do
       expect(current_path).to eq('/register')
       expect(page).to have_content("Name can't be blank and State can't be blank")
     end
+
+    it "cannot save form without unique email" do
+      user_1 = User.create({name: "Paul D",
+                address: "123 Main St.",
+                city: "Broomfield",
+                state: "CO",
+                zip: "80020",
+                email: "pauld@gmail.com",
+                password: "supersecure1"})
+
+      visit '/register'
+
+      new_user = ({name: "Paul D",
+                address: "123 Main St.",
+                city: "Broomfield",
+                state: "CO",
+                zip: "80020",
+                email: "pauld@gmail.com",
+                password: "supersecure1"})
+
+      fill_in :name, with: new_user[:name]
+      fill_in :address, with: new_user[:address]
+      fill_in :city, with: new_user[:city]
+      fill_in :state, with: new_user[:state]
+      fill_in :zip, with: new_user[:zip]
+      fill_in :email, with: "pauld@gmail.com"
+      fill_in :password, with: new_user[:password]
+      fill_in :confirm_password, with: new_user[:password]
+
+      click_button "Submit Form"
+
+      expect(current_path).to eq('/register')
+      expect(page).to have_content("Email has already been taken")
+      expect(page).to have_selector("input[value='Paul D']")
+      expect(page).to have_selector("input[value='123 Main St.']")
+      expect(page).to have_selector("input[value='Broomfield']")
+      expect(page).to have_selector("input[value='CO']")
+      expect(page).to have_selector("input[value='80020']")
+      save_and_open_page
+    end
   end
 end
