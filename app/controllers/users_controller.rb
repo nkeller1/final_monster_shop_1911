f62a07416b78198308e93856d954789faea5dabc
David Holtkamp <david.hholtkamp@gmail.com>
2020-02-20 14:50:20 -0700

    Move password update into own controller and update user model/controller
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.permit(:name, :address, :city, :state, :zip, :email, :password)
+      params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
     end
 end
