1266df599b2e2f09fe3ffaff16c33a7f5d324484
Paul Debevec <paulmdebevec@gmail.com>
2020-02-27 11:57:27 -0700

    Add BaseController to Merchant

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,4 +1,4 @@
-class Merchant::OrdersController < ApplicationController
+class Merchant::OrdersController < Merchant::BaseController
   def show
     @order = Order.find(params[:id])
     @item_orders = @order.merchant_items_on_order(current_user.merchant_id)
