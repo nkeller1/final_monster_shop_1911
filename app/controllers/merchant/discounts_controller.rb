ecbbbc50bebe845913717a9dc93c219027acacc0
Nathan Keller <keller.nathan@gmail.com>
2020-02-29 13:48:30 -0700

    Full CRU functionality.

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -3,6 +3,21 @@ class Merchant::DiscountsController < Merchant::BaseController
     @discounts = Discount.all
   end
 
+  def new
+  end
+
+  def create
+    @merchant = Merchant.find(current_user.merchant_id)
+    discount = @merchant.discounts.create(discount_params)
+    if discount.save
+      flash[:success] = "#{discount.name} Created Successfully"
+      redirect_to "/merchant/discounts"
+    else
+      flash[:error] = discount.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
   def edit
     @discount = Discount.find(params[:id])
   end
