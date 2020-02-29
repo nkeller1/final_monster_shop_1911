0a51cde2994e8e4fbafb364ee5399f48b15799d8
Nathan Keller <keller.nathan@gmail.com>
2020-02-29 11:27:24 -0700

    Create migration for discount table. Create model for discount. Update to schema

---
+++ db/migrate/20200229181430_create_discounts.rb
@@ -0,0 +1,12 @@
+class CreateDiscounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :discounts do |t|
+      t.string :name
+      t.integer :quantity_required
+      t.integer :percentage
+      t.references :merchant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
