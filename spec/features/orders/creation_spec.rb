8d1132de17a0d6e9a5b514c815c94fd6c43a3da9
Nathan Keller <keller.nathan@gmail.com>
2020-03-03 10:07:03 -0700

    Add test to apply discount to order creation

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe("Order Creation") do
 
     it 'I can create a new order' do
       click_on "Logout"
-      
+
       mike = Merchant.create(name: "Mike's Print Shop",
         address: '123 Paper Rd.',
         city: 'Denver',
@@ -138,6 +138,83 @@ RSpec.describe("Order Creation") do
 
       expect(page).to have_content("Please complete address form to create an order.")
       expect(page).to have_button("Create Order")
+    end
+
+  it "can create an order with discounts applied" do
+    click_on "Logout"
+
+    bike_shop = Merchant.create(
+      name: "Brian's Bike Shop",
+      address: '123 Bike Rd.',
+      city: 'Richmond',
+      state: 'VA',
+      zip: 11234)
+
+    merchant_user = User.create(
+      name: "Maria R",
+      address: "321 Notmain Rd.",
+      city: "Broomfield",
+      state: "CO",
+      zip: "80020",
+      email: "mariaaa@example.com",
+      password: "supersecure1",
+      role: 1,
+      merchant: bike_shop)
+
+    default_user_1 = User.create({
+      name: "Paul D",
+      address: "123 Main St.",
+      city: "Broomfield",
+      state: "CO",
+      zip: "80020",
+      email: "mariar@example.com",
+      password: "supersecure1",
+      role: 0
+      })
+
+    pencil = Item.create(
+       name: "Yellow Pencil",
+       description: "You can write on paper with it!",
+       price: 10,
+       image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg",
+       inventory: 100,
+       merchant: bike_shop)
+
+    discount_1 = Discount.create(
+      name: 'Pencil Discount',
+      quantity_required: 10,
+      percentage: 10,
+      merchant: bike_shop,
+      item: pencil)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(default_user_1)
+
+    visit "/items/#{pencil.id}"
+    click_on "Add To Cart"
+
+    visit "/cart"
+
+    9.times do
+      click_on "+"
+    end
+
+    click_on "Checkout"
+
+    fill_in :name, with: "Bert"
+    fill_in :address, with: "123 Sesame St."
+    fill_in :city, with: "NYC"
+    fill_in :state, with: "New York"
+    fill_in :zip, with: 10001
+
+    click_on "Create Order"
+
+    new_order = Order.last
+
+    click_link("#{new_order.id}")
+  
+    expect(page).to have_content("Grand Total: $90.0")
+    expect(page).to have_content("Subtotal: $90.0")
+    expect(page).to have_content("Item Price: $9.0")
     end
   end
 end
